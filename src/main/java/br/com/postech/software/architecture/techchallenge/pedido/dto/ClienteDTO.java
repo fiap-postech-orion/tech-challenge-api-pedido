ecec46265cbed15e7fd8bc93a53035a1214ef835
Yuri Nascimento <yuri.nascimento.ro@gmail.com>
2024-08-08 20:55:32 -0300

    ajuste clientedto

--- src/main/java/br/com/postech/software/architecture/techchallenge/pedido/dto/ClienteDTO.java
+++ src/main/java/br/com/postech/software/architecture/techchallenge/pedido/dto/ClienteDTO.java
@@ -9,6 +9,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.br.CPF;
 
+import java.text.Normalizer;
+
 @Data
 @Builder
 @AllArgsConstructor
@@ -16,14 +18,22 @@ import org.hibernate.validator.constraints.br.CPF;
 public class ClienteDTO {
 
 	private Long id;
+
+	@Size(min = 1, max = 250)
 	private String nome;
+
+	@Size(min = 1, max = 250)
 	@Email(message = "Email inválido, digite novamente", regexp = ".+[@].+[\\.].+")
 	private String email;
+
+	@Size(min = 1, max = 250)
 	@CPF(message="CPF inválido, digite novamente")
 	private String cpf;
+
 	@Size(min = 6, max = 20)
 	@NotNull
 	private String senha;
+
 	private Boolean status;
 
 	public ClienteDTO(Long id){
